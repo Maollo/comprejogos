414f8dbb125267cccd45c02d216bcaa296dae7d5
Maollo <43157476+Maollo@users.noreply.github.com>
2025-10-12 19:03:14 -0300

    first commit project

---
+++ src/main/java/maollo/comprejogos/ComprejogosApplication.java
@@ -0,0 +1,13 @@
+package maollo.comprejogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprejogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComprejogosApplication.class, args);
+	}
+
+}
